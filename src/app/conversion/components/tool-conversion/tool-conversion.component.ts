090404c128312b61cfb1c95f915ac6e7dd15670a
MauroBeltrame <maurobeltrame123@gmail.com>
2024-11-14 13:42:21 -0300

    copy to clipboard para ERA y agregue nombres a las monedas del conversor
    tambien agregue toFixed para limitar numero de decimales

--- src/app/conversion/components/tool-conversion/tool-conversion.component.ts
+++ src/app/conversion/components/tool-conversion/tool-conversion.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CurrencyERA } from '../../../interface/currency-era';
-import { FormBuilder, FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ExchangeRateService } from '../../../service/exchange-rate.service';
 
 @Component({
   selector: 'app-tool-conversion',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './tool-conversion.component.html',
   styleUrl: './tool-conversion.component.css'
 })
@@ -16,6 +16,176 @@ export class ToolConversionComponent implements OnInit {
   amount: number = 0;
   convertedAmount: number | null = null;
   currencykeys:string[]=[]
+  currencyNames: { [key: string]: string } = {
+    AED: "Dírham de los Emiratos Árabes Unidos",
+    AFN: "Afghani afgano",
+    ALL: "Lek albanés",
+    AMD: "Dram armenio",
+    ANG: "Florín antillano neerlandés",
+    AOA: "Kwanza angoleño",
+    ARS: "Peso argentino",
+    AUD: "Dólar australiano",
+    AWG: "Florín arubeño",
+    AZN: "Manat azerbaiyano",
+    BAM: "Marco convertible de Bosnia y Herzegovina",
+    BBD: "Dólar barbadense",
+    BDT: "Taka de Bangladés",
+    BGN: "Lev búlgaro",
+    BHD: "Dinar bareiní",
+    BIF: "Franco burundés",
+    BMD: "Dólar bermudeño",
+    BND: "Dólar de Brunéi",
+    BOB: "Boliviano",
+    BRL: "Real brasileño",
+    BSD: "Dólar bahameño",
+    BTN: "Ngultrum butanés",
+    BWP: "Pula botsuano",
+    BYN: "Rublo bielorruso",
+    BZD: "Dólar beliceño",
+    CAD: "Dólar canadiense",
+    CDF: "Franco congoleño",
+    CHF: "Franco suizo",
+    CLP: "Peso chileno",
+    CNY: "Yuan chino",
+    COP: "Peso colombiano",
+    CRC: "Colón costarricense",
+    CUP: "Peso cubano",
+    CVE: "Escudo caboverdiano",
+    CZK: "Corona checa",
+    DJF: "Franco yibutiano",
+    DKK: "Corona danesa",
+    DOP: "Peso dominicano",
+    DZD: "Dinar argelino",
+    EGP: "Libra egipcia",
+    ERN: "Nakfa eritreo",
+    ETB: "Birr etíope",
+    EUR: "Euro",
+    FJD: "Dólar fiyiano",
+    FKP: "Libra de las Islas Malvinas",
+    FOK: "Corona feroesa",
+    GBP: "Libra esterlina",
+    GEL: "Lari georgiano",
+    GGP: "Libra de Guernsey",
+    GHS: "Cedi ghanés",
+    GIP: "Libra de Gibraltar",
+    GMD: "Dalasi gambiano",
+    GNF: "Franco guineano",
+    GTQ: "Quetzal guatemalteco",
+    GYD: "Dólar guyanés",
+    HKD: "Dólar de Hong Kong",
+    HNL: "Lempira hondureño",
+    HRK: "Kuna croata",
+    HTG: "Gourde haitiano",
+    HUF: "Forinto húngaro",
+    IDR: "Rupia indonesia",
+    ILS: "Nuevo shéquel israelí",
+    IMP: "Libra manesa",
+    INR: "Rupia india",
+    IQD: "Dinar iraquí",
+    IRR: "Rial iraní",
+    ISK: "Corona islandesa",
+    JEP: "Libra de Jersey",
+    JMD: "Dólar jamaiquino",
+    JOD: "Dinar jordano",
+    JPY: "Yen japonés",
+    KES: "Chelín keniano",
+    KGS: "Som kirguís",
+    KHR: "Riel camboyano",
+    KID: "Dólar de Kiribati",
+    KMF: "Franco comorense",
+    KRW: "Won surcoreano",
+    KWD: "Dinar kuwaití",
+    KYD: "Dólar caimanés",
+    KZT: "Tenge kazajo",
+    LAK: "Kip laosiano",
+    LBP: "Libra libanesa",
+    LKR: "Rupia de Sri Lanka",
+    LRD: "Dólar liberiano",
+    LSL: "Loti lesotense",
+    LYD: "Dinar libio",
+    MAD: "Dírham marroquí",
+    MDL: "Leu moldavo",
+    MGA: "Ariary malgache",
+    MKD: "Denar macedonio",
+    MMK: "Kyat birmano",
+    MNT: "Tugrik mongol",
+    MOP: "Pataca de Macao",
+    MRU: "Ouguiya mauritano",
+    MUR: "Rupia mauricia",
+    MVR: "Rupia maldiva",
+    MWK: "Kwacha malauí",
+    MXN: "Peso mexicano",
+    MYR: "Ringgit malayo",
+    MZN: "Metical mozambiqueño",
+    NAD: "Dólar namibio",
+    NGN: "Naira nigeriana",
+    NIO: "Córdoba nicaragüense",
+    NOK: "Corona noruega",
+    NPR: "Rupia nepalesa",
+    NZD: "Dólar neozelandés",
+    OMR: "Rial omaní",
+    PAB: "Balboa panameño",
+    PEN: "Sol peruano",
+    PGK: "Kina papú",
+    PHP: "Peso filipino",
+    PKR: "Rupia pakistaní",
+    PLN: "Złoty polaco",
+    PYG: "Guaraní paraguayo",
+    QAR: "Rial catarí",
+    RON: "Leu rumano",
+    RSD: "Dinar serbio",
+    RUB: "Rublo ruso",
+    RWF: "Franco ruandés",
+    SAR: "Riyal saudí",
+    SBD: "Dólar de las Islas Salomón",
+    SCR: "Rupia seychelense",
+    SDG: "Libra sudanesa",
+    SEK: "Corona sueca",
+    SGD: "Dólar de Singapur",
+    SHP: "Libra de Santa Elena",
+    SLE: "Leone de Sierra Leona",
+    SOS: "Chelín somalí",
+    SRD: "Dólar surinamés",
+    SSP: "Libra sursudanesa",
+    STN: "Dobra de Santo Tomé y Príncipe",
+    SYP: "Libra siria",
+    SZL: "Lilangeni suazi",
+    THB: "Baht tailandés",
+    TJS: "Somoni tayiko",
+    TMT: "Manat turcomano",
+    TND: "Dinar tunecino",
+    TOP: "Paʻanga tongano",
+    TRY: "Lira turca",
+    TTD: "Dólar de Trinidad y Tobago",
+    TVD: "Dólar tuvaluano",
+    TWD: "Nuevo dólar taiwanés",
+    TZS: "Chelín tanzano",
+    UAH: "Grivna ucraniana",
+    UGX: "Chelín ugandés",
+    USD: "Dólar estadounidense",
+    UYU: "Peso uruguayo",
+    UZS: "Som uzbeko",
+    VES: "Bolívar soberano venezolano",
+    VND: "Dong vietnamita",
+    VUV: "Vatu vanuatuense",
+    WST: "Tala samoano",
+    XAF: "Franco CFA de África Central",
+    XCD: "Dólar del Caribe Oriental",
+    XDR: "Derechos especiales de giro",
+    XOF: "Franco CFA de África Occidental",
+    XPF: "Franco CFP",
+    YER: "Rial yemení",
+    ZAR: "Rand sudafricano",
+    ZMW: "Kwacha zambiano",
+    ZWL: "Dólar zimbabuense"
+  };
+  fb=inject(FormBuilder)
+  ers=inject(ExchangeRateService)
+  formulario=this.fb.nonNullable.group({
+    "baseCurrency":["",Validators.required],
+    "targetCurrency":["",Validators.required],
+    "amount":[0,Validators.required]
+  })
 
   
 
@@ -29,6 +199,11 @@ export class ToolConversionComponent implements OnInit {
   }
 
   convertCurrency(): void {
+    const formResults=this.formulario.getRawValue()
+    this.baseCurrency=formResults.baseCurrency
+    this.targetCurrency=formResults.targetCurrency
+    this.amount=formResults.amount
+
     this.ers.convert(this.baseCurrency, this.targetCurrency).subscribe((data) => {
       const rate = data.conversion_rate;
       this.convertedAmount = this.amount * rate;
@@ -36,7 +211,6 @@ export class ToolConversionComponent implements OnInit {
   }
 
 
-  fb=inject(FormBuilder)
-  ers=inject(ExchangeRateService)
+  
 
 }
