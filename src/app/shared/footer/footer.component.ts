cda7b47be62c366ad479dac7c55d1bbab3a6febb
nicolas <pavonnicolas11@gmail.com>
2024-11-20 16:39:17 -0300

    footer cambia ruteo

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -9,35 +9,32 @@ import { RouterModule } from '@angular/router';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent implements OnInit{
+  
   textButton: string = 'LogIn';  // Valor por defecto
   isLoggedIn: boolean = false;   // Estado de login
+  router = inject(Router);
 
   ngOnInit(): void {
     this.checkLoginStatus();
   }
 
   checkLoginStatus() {
-    // Comprobar el token en localStorage
-    const token = localStorage.getItem('token');
-    if (token && token !== '123.abc.!#$') {
-      this.textButton = 'Perfil';  // Si el token es válido, mostrar "Perfil"
-      this.isLoggedIn = true;
-    } else {
-      this.textButton = 'LogIn';  // Si no hay token, mostrar "LogIn"
-      this.isLoggedIn = false;
-    }
+    this.isLoggedIn = !!localStorage.getItem('token');
+    this.textButton = this.isLoggedIn ? 'Perfil' : 'LogIn';
+    // this.routerText = this.isLoggedIn ? 'profile' : 'login';
   }
+  
 
   toggleButton() {
     if (this.isLoggedIn) {
       // Redirigir al perfil
       // Aquí podrías navegar a la página de perfil, por ejemplo:
-      // this.router.navigate(['/profile']);
+      this.router.navigate(['/profile']);
       console.log('Ir al perfil');
     } else {
       // Redirigir al login
       // Aquí podrías navegar a la página de login, por ejemplo:
-      // this.router.navigate(['/login']);
+      this.router.navigate(['/login']);
       console.log('Ir al login');
     }
   }
